9b17289bf41184f13bb923e3e2baa681dc778b08
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-02-04 17:06:34 +0530

    Added Nested file Day-9

---
+++ Nesting.py
@@ -0,0 +1,26 @@
+#Nesting dictionary in dictionary
+
+travel_vlog={
+    
+    "France": {"cities_visited" :["paris","Lilon","Dijon"],"total_visits":21 },
+
+    "Germany": {"cities_visited" :["Berlin","Hamburg","stutgart"],"total_visits":5 },
+
+}
+
+#Nesting Dictionary in a list
+
+travel_vlog=[
+    {
+      "Country":"France",
+      "cities_visited":["paris","Lilon","Dijon"],
+      "total_visits":21
+    },
+     
+    {
+        "Country":"Germany",
+        "cities_visited" :["Berlin","Hamburg","stutgart"],
+        "total_visits":5 
+    },
+
+]
